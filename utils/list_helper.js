99b3dd54fda2681e58afa32f28eccd16c0b4c3e8
Anton Taleiko <anton.taleiko@gmail.com>
2021-04-01 17:28:45 +0300

    4.5, previous commit was 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,15 +2,27 @@ const dummy = (/*blogs*/) => {
   return 1
 }
 
-const totalLikes = ( blogs ) => {
+const totalLikes = (blogs) => {
   if (blogs.length === 0) {
     return 0
-  } else if  (blogs.length === 1) {
-    return blogs[0].likes
   }
   return blogs.reduce((sum, nextBlog) => sum + nextBlog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+  const favorite = blogs.reduce((prev, current) => {
+    return (prev.likes > current.likes) ? prev : current
+  })
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
