e1559911a8a8f6f133ccd9239d5c3161c8466c00
Anton Taleiko <anton.taleiko@gmail.com>
2021-04-01 14:41:53 +0300

    4.2: All files created, need to start changing index.js

---
+++ controllers/blogs.js
@@ -0,0 +1,22 @@
+const blogsRouter = require('express').Router()
+const Note = require('../models/note')
+
+app.get('/api/blogs', (request, response) => {
+    Blog
+      .find({})
+      .then(blogs => {
+        response.json(blogs)
+      })
+  })
+  
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+    response.status(201).json(result)
+    })
+})
+
+module.exports = blogsRouter
