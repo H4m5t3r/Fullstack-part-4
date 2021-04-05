e08a848fdc5fc89c15959954f92de6bbcd812693
Anton Taleiko <anton.taleiko@gmail.com>
2021-04-05 15:17:24 +0300

    4.20

--- utils/middleware.js
+++ utils/middleware.js
@@ -30,7 +30,10 @@ const errorHandler = (error, request, response, next) => {
 }
 
 const tokenExtractor = (request, response, next) => {
-
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  }
   next()
 }
 
